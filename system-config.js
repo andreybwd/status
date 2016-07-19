094575aea5b3db10997d89e1c705cd2249af8ac3
Andrew Mukhin <andreybwd@gmail.com>
2016-07-19 04:06:13 +0300

    deploy(dist): deploy on GitHub pages

---
+++ system-config.js
@@ -0,0 +1,58 @@
+// SystemJS configuration file, see links for more information
+// https://github.com/systemjs/systemjs
+// https://github.com/systemjs/systemjs/blob/master/docs/config-api.md
+/***********************************************************************************************
+ * User Configuration.
+ **********************************************************************************************/
+/** Map relative paths to URLs. */
+var map = {};
+/** User packages configuration. */
+var packages = {};
+////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************************
+ * Everything underneath this line is managed by the CLI.
+ **********************************************************************************************/
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/forms',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    // Thirdparty barrels.
+    'rxjs',
+    // App specific barrels.
+    'app',
+    'app/shared',
+    'app/workplace',
+    'app/discount',
+    'app/examination-list',
+    'app/examination-list/examination',
+    'app/examination',
+    'app/examination/list/examination-list',
+    'app/examination/examination-list',
+    'app/doctors',
+    'app/doctor',
+];
+var cliSystemConfigPackages = {};
+barrels.forEach(function (barrelName) {
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+cliSystemConfigPackages['vendor/ng2-bootstrap'] = { defaultExtension: 'js' };
+// Apply the CLI SystemJS configuration.
+System.config({
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js',
+        'moment': 'vendor/moment/moment.js',
+        'ng2-bootstrap': 'vendor/ng2-bootstrap/ng2-bootstrap.js'
+    },
+    packages: cliSystemConfigPackages
+});
+// Apply the user's configuration.
+System.config({ map: map, packages: packages });
+//# sourceMappingURL=system-config.js.map
